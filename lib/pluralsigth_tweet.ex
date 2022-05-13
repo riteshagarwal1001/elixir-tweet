a125ef91cc4f27efac74a1fad8ed1f6d4d79e9cd
Ritesh Agarwal <RiteshA@talentica-all.com>
2022-05-13 14:52:07 +0530

    initial set up elixir
    mix new pluralsigth_tweet --sup

---
+++ lib/pluralsigth_tweet.ex
@@ -0,0 +1,18 @@
+defmodule PluralsigthTweet do
+  @moduledoc """
+  Documentation for `PluralsigthTweet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PluralsigthTweet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pluralsigth_tweet_test.exs
@@ -0,0 +1,8 @@
+defmodule PluralsigthTweetTest do
+  use ExUnit.Case
+  doctest PluralsigthTweet
+
+  test "greets the world" do
+    assert PluralsigthTweet.hello() == :world
+  end
+end
